ea67fbf4d537e7a9fd5487cec2b40246779c8a95
Matthias Diester <matthias.diester@de.ibm.com>
2024-07-03 16:27:22 +0200

    Update `.buildenv` Node.js version to 18
    Mitigate issue with Node.js version 16 being unsupported in newer Buildpacks based builds.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-if (process.version.startsWith('v16.')) {
-    console.log('This runs with the expected Node version 16.');
+if (process.version.startsWith('v18.')) {
+    console.log('This runs with the expected Node version 18.');
 } else {
-    console.error(`I am expecting Node version 16, but I am running ${process.version}.`);
+    console.error(`I am expecting Node version 18, but I am running ${process.version}.`);
     process.exit(1);
 }
