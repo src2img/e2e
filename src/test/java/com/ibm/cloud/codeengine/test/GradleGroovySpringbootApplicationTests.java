9acd8c29d87b9049e0f9d64c65b3acdb38249060
Sascha Schwarze <schwarzs@de.ibm.com>
2024-11-05 10:35:58 +0100

    Regenerate using latest Spring and Gradle versions

---
+++ src/test/java/com/ibm/cloud/codeengine/test/GradleGroovySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.cloud.codeengine.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleGroovySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
